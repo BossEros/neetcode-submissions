7bf67e77db3325b945b0d8cc0ce6b2b8ae4791e9
Christian Dave Vilan <136943421+BossEros@users.noreply.github.com>
2026-06-06 23:28:51 +0800

    Add: is-anagram - submission-18

---
+++ Data Structures & Algorithms/is-anagram/submission-18.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(s.Length != t.Length)
+            return false;
+
+        char[] firstWord = s.ToCharArray();
+        char[] secondWord = t.ToCharArray();
+        Array.Sort(firstWord);
+        Array.Sort(secondWord);
+
+        return firstWord.SequenceEqual(secondWord);
+    }
+}
